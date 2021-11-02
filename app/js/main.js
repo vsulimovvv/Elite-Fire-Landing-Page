e58e7b1a4cf4d15173269aa7005023b04111b493
vsulimovvv <super.sulimov2014@yandex.ru>
2021-11-02 22:33:40 +0300

    Do something

--- app/js/main.js
+++ app/js/main.js
@@ -47,7 +47,7 @@ function construcror() {
     //   }
     // });
 
- 
+
     // ₽
     rangeSlider.addEventListener('input', e => {
       length.textContent = rangeSlider.value;
@@ -63,6 +63,50 @@ function construcror() {
 
   rangeSliderLength();
 }
+const rangeSliderForPrice = () => {
+  const rangeSlider = document.querySelectorAll('.range-slider');
+
+  rangeSlider.forEach(item => {
+    if (item) {
+      noUiSlider.create(item, {
+        start: [100, 1000],
+        connect: true,
+        step: 1,
+        range: {
+          'min': [50],
+          'max': [1200]
+        }
+      });
+
+      const input0 = document.getElementById('input-0');
+      const input1 = document.getElementById('input-1');
+
+      const inputs = [input0, input1];
+
+      item.noUiSlider.on('update', function (values, handle) {
+        inputs[handle].value = Math.round(values[handle]);
+      });
+
+      const setRangeSlider = (i, value) => {
+        let arr = [null, null];
+        arr[i] = value;
+
+        item.noUiSlider.set(arr);
+      };
+
+      inputs.forEach((el, index) => {
+        el.addEventListener('change', e => {
+          setRangeSlider(index, e.currentTarget.value);
+        });
+        el.addEventListener('input', e => {
+          setRangeSlider(index, e.currentTarget.value);
+        });
+      });
+    };
+  });
+};
+rangeSliderForPrice();
+
 
 // 
 // function updateCount() {
