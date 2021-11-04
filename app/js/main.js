78e5fa431a4b08ea79a324e821c8ed1e450cc702
vsulimovvv <super.sulimov2014@yandex.ru>
2021-11-04 18:53:27 +0300

    did something great

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 const swiper = new Swiper(".face-panel__box", {
-  slidesPerView: 1,
+  slidesPerView: 'auto',
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -17,133 +17,68 @@ const swipers = new Swiper(".slider", {
   },
 });
 
-// * LengthSlider
-// rangeSliderLength();
-
 construcror();
 
 function construcror() {
-  function rangeSliderLength() {
-    const rangeSlider = document.querySelector('.input-range');
-    const length = document.querySelector('.length-output');
-    const fill = document.querySelector('.fill');
-    const totalPriceEl = document.querySelector('#total-price');
-    const checkInputs = document.querySelectorAll('.check__input');
-    // console.log(checkInputs)
+  const construcrorEl = document.querySelector('.constructor__details');
+  const totalPriceEl = document.querySelector('#total-price');
+  // const inputs = document.querySelectorAll('.check__input');
+  const length = document.querySelector('.length-output');
+  const fill = document.querySelector('.fill');
+  const rangeSlider = document.querySelector('.input-range');
 
-    // let totalPrice = 0;
+  function setFilBg() {
+    fill.style.width = rangeSlider.value * 10 + '%'
+  }
+  setFilBg();
+  
+  totalPriceEl.textContent = rangeSlider.value * 10000;
+  //    
+  // function updateCount() {
+  //   const totalPrice = document.querySelector('#total-price');
+  //   let speed = 500;
 
-    function setFilBg() {
-      fill.style.width = rangeSlider.value * 10 + '%'
+  //   let target = +rangeSlider.value * 10900;
+  //   let count = parseInt(totalPrice.textContent);
+  //   console.log('targetValue', target);
+  //   console.log('count', count);
+
+  //   const inc = target / speed;
+
+  //   if (count < target) {
+  //     // speed = 1500;
+  //     totalPrice.textContent = `${Math.ceil(count + inc)} ₽`;
+  //     console.log(totalPrice)
+  //     setTimeout(updateCount, 1);
+  //   } else {
+  //     count.textContent = `${+target}`;
+  //   }
+  // }
+
+  // updateCount()
+  construcrorEl.addEventListener('input', e => {
+    setFilBg();
+    // 
+    // updateCount()
+    // 
+    let currentValue = e.target.value;
+    let priceFromRange = 10000;
+
+    if (e.target.classList.contains('input-range')) {
+      length.textContent = currentValue;
+
+      price = currentValue * priceFromRange;
     }
 
-    totalPriceEl.textContent = `${rangeSlider.value * 10000} ₽`;
-    // totalPrice = `${rangeSlider.value * 10000} ₽`
-    // checkInputs.forEach(input => {
-    //   console.log(input.checked)
-    //   if (input.checked) {
-    //     // totalPrice + 5000
-    //     totalPrice += 5000;
-    //   }
-    // });
-
-
-    // ₽
-    rangeSlider.addEventListener('input', e => {
-      length.textContent = rangeSlider.value;
-      totalPriceEl.textContent = `${rangeSlider.value * 10000} ₽`;
-
-
-      // totalPriceEl.textContent = totalPrice;
-
-      // totalPriceEl.textContent
-      setFilBg();
-    });
-  };
-
-  rangeSliderLength();
+    totalPriceEl.textContent = price;
+  });
 }
 
-const rangeSliderForPrice = () => {
-  const rangeSlider = document.querySelectorAll('.range-slider');
 
-  rangeSlider.forEach(item => {
-    if (item) {
-      noUiSlider.create(item, {
-        start: [100, 1000],
-        connect: true,
-        step: 1,
-        range: {
-          'min': [50],
-          'max': [1200]
-        }
-      });
-
-      const input0 = document.getElementById('input-0');
-      const input1 = document.getElementById('input-1');
-
-      const inputs = [input0, input1];
-
-      item.noUiSlider.on('update', function (values, handle) {
-        inputs[handle].value = Math.round(values[handle]);
-      });
-
-      const setRangeSlider = (i, value) => {
-        let arr = [null, null];
-        arr[i] = value;
-
-        item.noUiSlider.set(arr);
-      };
-
-      inputs.forEach((el, index) => {
-        el.addEventListener('change', e => {
-          setRangeSlider(index, e.currentTarget.value);
-        });
-        el.addEventListener('input', e => {
-          setRangeSlider(index, e.currentTarget.value);
-        });
-      });
-    };
-  });
-};
-rangeSliderForPrice();
 
 
 // 
-// function updateCount() {
-//   const totalPrice = document.querySelector('#total-price');
-//   let count = parseInt(totalPrice.textContent);
-//   let speed = 250;
 
-//   let targetValue = `${rangeSlider.value * 10000}`
-//   console.log('targetValue', targetValue)
-//   console.log('count', count)
-
-//   const inc = +targetValue / speed;
-//   console.log(inc)
-
-// if (count < +targetValue) {
-//   totalPrice.textContent = `${Math.ceil(count + inc)} ₽`;
-//   setTimeout(updateCount, 1);
-// } else if (count >= +targetValue) {
-//   totalPrice.textContent = `${Math.ceil(count - inc)} ₽`;
-//   count = `${Math.ceil(count - inc)} ₽`;
-//   setTimeout(updateCount, 1);
-// } else {
-
-// count.textContent = `${+targetValue}`;
-// }
-
-// if (count < +targetValue) {
-//   totalPrice.textContent = `${Math.ceil(count + inc)} ₽`;
-//   setTimeout(updateCount, 1);
-// } else {
-
-//   count.textContent = `${+targetValue}`;
-// }
-
-
-// updateCount();
 
 const showMenu = () => {
   const menuBtn = document.querySelector('.menu__toggle');
@@ -174,45 +109,4 @@ const showMenu = () => {
     });
   })
 };
-showMenu();
-
-// const modals = () => {
-//   function bindModal(openBtn, modal, close, overlay = 'overaly') {
-//     const openBtnEl = document.querySelector(openBtn);
-//     const modalEl = document.querySelector(modal);
-//     const closeEl = document.querySelector(close);
-//     const overlayEl = document.querySelector(overlay);
-//     const body = document.querySelector('body');
-
-
-//     if (modalEl) {
-//       openBtnEl.addEventListener('click', e => {
-//         if (e.target) {
-//           e.preventDefault()
-//         }
-
-//         modalEl.classList.add('active');
-//         overlayEl.classList.add('active');
-//         body.classList.add('no-scroll');
-//       });
-
-//       closeEl.addEventListener('click', e => {
-//         modalEl.classList.remove('active');
-//         overlayEl.classList.remove('active');
-//         body.classList.remove('no-scroll');
-//       });
-
-//       modalEl.addEventListener('click', e => {
-//         if (e.target === modalEl) {
-//           modalEl.classList.remove('active');
-//           overlayEl.classList.remove('active');
-//           body.classList.remove('no-scroll');
-//         }
-//       })
-//     };
-//   };
-
-//   bindModal('.menu', '.menu__toggle', '.cart__close', '.overlay');
-
-// };
-// modals();
+// showMenu();
