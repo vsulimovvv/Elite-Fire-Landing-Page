aacc2ed971c987d193a9dad62f7491feb735e7fb
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-14 20:55:15 +0300

    Added popups

--- app/js/main.js
+++ app/js/main.js
@@ -90,5 +90,46 @@ window.addEventListener('DOMContentLoaded', () => {
       scrollTopEl.classList.remove('show-scroll');
     }
   }
+
+  // * ==== Modal
+  (function modals() {
+    function bindModal(openBtn, modal, close) {
+      const openBtnEl = document.querySelectorAll(openBtn);
+      const modalEl = document.querySelector(modal);
+      const closeEl = document.querySelectorAll(close);
+      const body = document.querySelector('body');
+
+      if (modalEl) {
+        openBtnEl.forEach(el => {
+          el.addEventListener('click', e => {
+
+            if (e.target) {
+              e.preventDefault()
+            }
+
+            modalEl.classList.add('active');
+            body.classList.add('no-scroll');
+          });
+        })
+
+        closeEl.forEach(btn => {
+          btn.addEventListener('click', e => {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          });
+        })
+
+        modalEl.addEventListener('click', e => {
+          if (e.target === modalEl) {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          }
+        })
+      };
+    };
+    bindModal('.btn-back-call', '.popup--back-call', '.popup__close');
+    bindModal('.btn-request', '.popup--request', '.popup__close');
+  }());
+
   window.addEventListener('scroll', scrollTop);
 })
