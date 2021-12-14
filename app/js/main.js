c3a7655780e326cd0e27e9d5a9cd0a560558b742
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-14 17:33:01 +0300

    Added dist

--- app/js/main.js
+++ app/js/main.js
@@ -1,92 +1,94 @@
-const swiper = new Swiper(".face-panel__box", {
-  slidesPerView: 'auto',
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+window.addEventListener('DOMContentLoaded', () => {
+  AOS.init();
+// * ==== Show Menu
+  (function showMenu() {
+    const menuBtn = document.querySelector('.menu__toggle');
+    const menu = document.querySelector('.menu__list');
+    const body = document.querySelector('body');
+    const overlay = document.querySelector('.overlay');
+    const links = document.querySelectorAll('.menu__link');
 
-const swipers = new Swiper(".slider", {
-  slidesPerView: 'auto',
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    menuBtn.addEventListener('click', e => {
+      menu.classList.toggle('active');
+      menuBtn.classList.toggle('active');
+      overlay.classList.toggle('active');
+      body.classList.toggle('no-scroll');
+    });
 
-construcror();
-
-function construcror() {
-  const construcrorEl = document.querySelector('.constructor__details');
-  const totalPriceEl = document.querySelector('#total-price');
-  // const inputs = document.querySelectorAll('.check__input');
-  const length = document.querySelector('.length-output');
-  const fill = document.querySelector('.fill');
-  const rangeSlider = document.querySelector('.input-range');
-
-  function setFilBg() {
-    fill.style.width = rangeSlider.value * 10 + '%'
-  }
-  setFilBg();
-
-  totalPriceEl.textContent = rangeSlider.value * 10000;
-
-  construcrorEl.addEventListener('input', e => {
-    setFilBg();
-    let currentValue = e.target.value;
-    let priceFromRange = 10000;
-
-    if (e.target.classList.contains('input-range')) {
-      length.textContent = currentValue;
-
-      price = currentValue * priceFromRange;
-    }
-
-    totalPriceEl.textContent = price;
-  });
-}
-
-const showMenu = () => {
-  const menuBtn = document.querySelector('.menu__toggle');
-  const menu = document.querySelector('.menu__list');
-  const body = document.querySelector('body');
-  const overlay = document.querySelector('.overlay');
-  const links = document.querySelectorAll('.menu__link');
-
-  menuBtn.addEventListener('click', e => {
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('active');
-    overlay.classList.toggle('active');
-    body.classList.toggle('no-scroll');
-  });
-
-  overlay.addEventListener('click', e => {
-    menu.classList.remove('active');
-    menuBtn.classList.remove('active');
-    overlay.classList.remove('active');
-    body.classList.remove('no-scroll');
-  });
-  links.forEach(l => {
-    l.addEventListener('click', e => {
+    overlay.addEventListener('click', e => {
       menu.classList.remove('active');
       menuBtn.classList.remove('active');
       overlay.classList.remove('active');
       body.classList.remove('no-scroll');
     });
-  })
-};
-showMenu();
+    links.forEach(l => {
+      l.addEventListener('click', e => {
+        menu.classList.remove('active');
+        menuBtn.classList.remove('active');
+        overlay.classList.remove('active');
+        body.classList.remove('no-scroll');
+      });
+    })
+  })();
 
-//* Show scroll top
-function scrollTop() {
-  const scrollTopEl = document.getElementById('scroll-top');
-  if (this.scrollY >= 560) {
-    scrollTopEl.classList.add('show-scroll');
-  } else {
-    scrollTopEl.classList.remove('show-scroll');
+  const swiper = new Swiper(".face-panel__box", {
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  const swipers = new Swiper(".slider", {
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  construcror();
+
+  function construcror() {
+    const construcrorEl = document.querySelector('.constructor__details');
+    const totalPriceEl = document.querySelector('#total-price');
+    const length = document.querySelector('.length-output');
+    const fill = document.querySelector('.fill');
+    const rangeSlider = document.querySelector('.input-range');
+
+    function setFilBg() {
+      fill.style.width = rangeSlider.value * 10 + '%'
+    }
+    setFilBg();
+
+    totalPriceEl.textContent = rangeSlider.value * 10000;
+
+    construcrorEl.addEventListener('input', e => {
+      setFilBg();
+      let currentValue = e.target.value;
+      let priceFromRange = 10000;
+
+      if (e.target.classList.contains('input-range')) {
+        length.textContent = currentValue;
+
+        price = currentValue * priceFromRange;
+      }
+
+      totalPriceEl.textContent = price;
+    });
   }
-}
-window.addEventListener('scroll', scrollTop);
+
+  //* Show scroll top
+  function scrollTop() {
+    const scrollTopEl = document.getElementById('scroll-top');
+    if (this.scrollY >= 560) {
+      scrollTopEl.classList.add('show-scroll');
+    } else {
+      scrollTopEl.classList.remove('show-scroll');
+    }
+  }
+  window.addEventListener('scroll', scrollTop);
+})
